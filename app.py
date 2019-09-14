080736d1477f14a1cf088925db1446acfaa428d7
ryuto <rryuto5210@gmail.com>
2019-09-15 01:28:19 +0900

    VERSION 1.0

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, request, jsonify, render_template, url_for, send_from_directory, session
+# -*- coding: utf-8 -*-
+
+from flask import Flask, redirect, make_response, request, jsonify, render_template, url_for, send_from_directory, session
 from keras import models
 from PIL import Image
 from keras.models import load_model
@@ -12,6 +14,7 @@ import glob
 import tensorflow as tf
 from keras.models import model_from_json
 from werkzeug import secure_filename
+import base64
 
 # IOError: image file is truncated (0 bytes not processed)回避のため
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -19,79 +22,171 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 app = Flask(__name__)
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-SEEDS_FOLDER = './seeds'
-app.config['SEEDS_FOLDER'] = SEEDS_FOLDER
 app.config['SECRET_KEY'] = os.urandom(24)
 
 
 CORS(app)
 
-# http://127.0.0.1:5000/にアクセスしたら、一番最初に読み込まれるページ
 @app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
-# 画像を選択し、アップロードボタンを押したら呼ばれる処理
 @app.route('/predict', methods=['GET','POST'])
 def predict():
-    # リクエストがPOSTの時にのみ、条件文の中のコードが実行されます。
     if request.method == "POST":
         if 'file' not in request.files:
             print("ファイルがありません")
         else:
             img = request.files["file"]
             filename = secure_filename(img.filename)
-            img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            img_url = '/uploads/' + filename
 
-            graph = tf.get_default_graph()
-            backend.clear_session() # 2回以上連続してpredictするために必要な処理
+            root, ext = os.path.splitext(filename)
+            ext = ext.lower()
+
+            gazouketori = set([".jpg", ".jpeg", ".jpe", ".jp2", ".png", ".webp", ".bmp", ".pbm", ".pgm", ".ppm",
+                      ".pxm", ".pnm",  ".sr",  ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic", ".dib"])
+            if ext not in gazouketori:
+                return render_template('index.html',massege = "対応してない拡張子です",color = "red")
+            print("success")
+            try:
+
+                # img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # img_url = '/uploads/' + filename
+
+                graph = tf.get_default_graph()
+                backend.clear_session() # 2回以上連続してpredictするために必要な処理
+
+                # モデルの読み込み
+                model = model_from_json(open('and.json', 'r').read())
+
+                # 重みの読み込み
+                model.load_weights('and_weight.hdf5')
 
 
-            # モデルの読み込み
-            model = model_from_json(open('and_1.json', 'r').read())
+                image_size = 50
 
-            # 重みの読み込み
-            model.load_weights('and_1_weight.hdf5')
+                image = Image.open(img)
+                image = image.convert("RGB")
+                image = image.resize((image_size, image_size))
+                data = np.asarray(image)
+                X = np.array(data)
+                X = X.astype('float32')
+                X = X / 255.0
+                X = X[None, ...]
+
+                prd = model.predict(X)
+                other_labels = np.argsort(prd)[0][::-1][:3]
+                other_pros = [prd[0][other_labels[0]], prd[0][other_labels[1]], prd[0][other_labels[2]]]
+
+                details = [
+                       '水温90℃を限度に、洗濯機で洗えます。',
+                       '水温50℃を限度に、洗濯機で洗えます。',
+                       'ハンガー等を使って、つり干しします。',
+                       '漂白できません。',
+                       '弱い操作による、ウエットクリーニングができます。',
+                       '水温60℃を限度に、洗濯機で洗えます。',
+                       'ウエットクリーニングができます。',
+                       'ドライクリーニングはできません。',
+                       '非常に弱い操作による、ウエットクリーニングができます。',
+                       '水温30℃を限度に、洗濯機で非常に弱い洗濯ができます。',
+                       '水温40℃を限度に、洗濯機で非常に弱い洗濯ができます。',
+                       '200℃を限度に、アイロンが使えます。',
+                       'ご家庭では洗えません。',
+                       'ウエットクリーニングはできません。',
+                       '石油系溶剤による、ドライクリーニングができます。',
+                       '塩素系・酸素系漂白剤で、漂白できます。',
+                       '日陰で、平干しします。',
+                       '石油系溶剤による、弱いドライクリーニングができます。',
+                       '脱水せずぬれたまま、平干しします。',
+                       'パークロロエチレン及び石油系溶剤による、弱いドライクリーニングができます。',
+                       '排気温度60℃を上限に、タンブル乾燥できます。',
+                       '150℃を限度に、アイロンが使えます。',
+                       'タンブル乾燥禁止です。',
+                       '水温30℃を限度に、洗濯機で洗えます。',
+                       '脱水せずぬれたまま、つり干しします。',
+                       '日陰で、脱水せずぬれたまま平干しします。',
+                       '水温40℃を限度に、洗濯機で弱い洗濯ができます。',
+                       'アイロンは使えません。',
+                       '平干しします。',
+                       '水温30℃を限度に、洗濯機で弱い洗濯ができます。',
+                       '日陰で、つり干しします。',
+                       '酸素系漂白剤で、漂白できます。塩素系ではできません。',
+                       '水温30℃を限度に、洗濯機で弱い洗濯ができます。',
+                       'パークロロエチレン及び石油系溶剤による、ドライクリーニングができます。',
+                       '水温40℃を限度に、手洗いできます。',
+                       '水温70℃を限度に、洗濯機で洗えます。',
+                       '日陰で、脱水せずぬれたままつり干しします。',
+                       '排気温度80℃を上限に、タンブル乾燥できます。',
+                       '水温30℃を限度に、洗濯機で洗えます',
+                       '水温50℃を限度に、洗濯機で弱い洗濯ができます。',
+                       '110℃を限度に、スチームなしでアイロンが使えます。']
+
+                icons = ['https://shopping.geocities.jp/ecoloco/images/sentaku-images/95c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/50c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/turiboshi.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/ensosanso-hyouhaku-3.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/cwetw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/60c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/cwet.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/dry-ng.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/cwetww.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/30cww.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/40cww.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/airon200c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/ng.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/cwet-ng.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/fdry.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/ensosanso-hyouhaku-2.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/hiraboshi-hikage.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/fdryw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/hiraboshi-hikage.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/pfdryw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/tanb60c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/airon150c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/tanb-ng.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/30c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/turiboshi-nure.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/hiraboshi-hikagenure.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/40cw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/airon-ng.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/hiraboshi.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/60cw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/turiboshi-hikage.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/sanso-hyouhaku-2.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/30cw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/pfdry.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/hand40c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/70c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/turiboshi-nurehikage.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/tanb80c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/40c.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/50cw.gif',
+                        'https://shopping.geocities.jp/ecoloco/images/sentaku-images/airon110c.gif']
 
 
-            image_size = 50
+                pre1_img_url = icons[other_labels[0]]
+                pre1_detail = details[other_labels[0]]
+                pre1_pro = str(round(other_pros[0] * 100)) + '%'
 
+                pre2_img_url = icons[other_labels[1]]
+                pre2_detail = details[other_labels[1]]
+                pre2_pro = str(round(other_pros[1] * 100)) + '%'
+
+                pre3_img_url = icons[other_labels[2]]
+                pre3_detail = details[other_labels[2]]
+                pre3_pro = str(round(other_pros[2] * 100)) + '%'
+
+            except:
+                return render_template('index.html',massege = "解析出来ませんでした",color = "red")
+
+            buf = io.BytesIO()
             image = Image.open(img)
-            image = image.convert("RGB")
-            image = image.resize((image_size, image_size))
-            data = np.asarray(image)
-            X = np.array(data)
-            X = X.astype('float32')
-            X = X / 255.0
-            X = X[None, ...]
+            image.save(buf, 'png')
+            qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
+            qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
 
-            # 受け取った画像をnumpyのアレイに変換
-            prd = model.predict(X)
-            # 配列の最大要素のインデックスを返しprelabelに代入します
-            prelabel = np.argmax(prd, axis=1)
-            probability = max(prd[0])
-
-            if prelabel == 0:
-                name = "40_weak"
-                pre_img_url = "/seeds/wash_40.png"
-                pre_detail = '水温40℃を限度に、洗濯機で非常に弱い洗濯ができます。'
-            elif prelabel == 1:
-                name = "donot_tumble_dry"
-                pre_img_url = "/seeds/dryer_no.png"
-                pre_detail = 'ウエットクリーニングはできません。'
-            elif prelabel == 2:
-                name = "ironing_upto150"
-                pre_img_url = "/seeds/iron_150.png"
-                pre_detail = '150℃を限度に、アイロンが使えます。'
-            elif prelabel == 3:
-                name = "not_bleachable"
-                pre_img_url = "/seeds/not_bleachable.png"
-                pre_detail = '漂白できません。'
-
-            return render_template('index.html',name=name, img_url=img_url, probability=probability, pre_img_url=pre_img_url, pre_detail=pre_detail )
+            return render_template('index.html', img=qr_b64data, pre1_img_url=pre1_img_url, pre1_detail=pre1_detail, pre1_pro=pre1_pro, pre2_img_url=pre2_img_url, pre2_detail=pre2_detail, pre2_pro=pre2_pro, pre3_img_url=pre3_img_url, pre3_detail=pre3_detail, pre3_pro=pre3_pro)
     else:
-        # ターミナル及びコマンドプロンプトに出力するメッセージ
         print("get request")
 
     return render_template('index.html')
@@ -100,9 +195,18 @@ def predict():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/seeds/<filename>')
-def seed_file(filename):
-    return send_from_directory(app.config['SEEDS_FOLDER'], filename)
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+@app.errorhandler(413)
+def oversize(error):
+    return render_template('index.html',massege = "画像サイズが大きすぎます",color = "red")
+@app.errorhandler(400)
+def nosubmit(error):
+    return render_template('index.html',massege = "画像を送信してください",color = "red")
+@app.errorhandler(503)
+def all_error_handler(error):
+     return 'InternalServerError\n', 503
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    app.run()
---
+++ pre4/app-pre4.py
@@ -0,0 +1,108 @@
+from flask import Flask, redirect, request, jsonify, render_template, url_for, send_from_directory, session
+from keras import models
+from PIL import Image
+from keras.models import load_model
+from flask_cors import CORS
+from PIL import ImageFile
+from keras.backend import tensorflow_backend as backend
+import keras
+import numpy as np
+import sys, os, io
+import glob
+import tensorflow as tf
+from keras.models import model_from_json
+from werkzeug import secure_filename
+
+# IOError: image file is truncated (0 bytes not processed)回避のため
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+SEEDS_FOLDER = './seeds'
+app.config['SEEDS_FOLDER'] = SEEDS_FOLDER
+app.config['SECRET_KEY'] = os.urandom(24)
+
+
+CORS(app)
+
+# http://127.0.0.1:5000/にアクセスしたら、一番最初に読み込まれるページ
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+# 画像を選択し、アップロードボタンを押したら呼ばれる処理
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    # リクエストがPOSTの時にのみ、条件文の中のコードが実行されます。
+    if request.method == "POST":
+        if 'file' not in request.files:
+            print("ファイルがありません")
+        else:
+            img = request.files["file"]
+            filename = secure_filename(img.filename)
+            img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img_url = '/uploads/' + filename
+
+            graph = tf.get_default_graph()
+            backend.clear_session() # 2回以上連続してpredictするために必要な処理
+
+
+            # モデルの読み込み
+            model = model_from_json(open('and_1.json', 'r').read())
+
+            # 重みの読み込み
+            model.load_weights('and_1_weight.hdf5')
+
+
+            image_size = 50
+
+            image = Image.open(img)
+            image = image.convert("RGB")
+            image = image.resize((image_size, image_size))
+            data = np.asarray(image)
+            X = np.array(data)
+            X = X.astype('float32')
+            X = X / 255.0
+            X = X[None, ...]
+
+            # 受け取った画像をnumpyのアレイに変換
+            prd = model.predict(X)
+            # 配列の最大要素のインデックスを返しprelabelに代入します
+            prelabel = np.argmax(prd, axis=1)
+            probability = max(prd[0])
+
+            if prelabel == 0:
+                name = "40_weak"
+                pre_img_url = "/seeds/wash_40.png"
+                pre_detail = '水温40℃を限度に、洗濯機で非常に弱い洗濯ができます。'
+            elif prelabel == 1:
+                name = "donot_tumble_dry"
+                pre_img_url = "/seeds/dryer_no.png"
+                pre_detail = 'ウエットクリーニングはできません。'
+            elif prelabel == 2:
+                name = "ironing_upto150"
+                pre_img_url = "/seeds/iron_150.png"
+                pre_detail = '150℃を限度に、アイロンが使えます。'
+            elif prelabel == 3:
+                name = "not_bleachable"
+                pre_img_url = "/seeds/not_bleachable.png"
+                pre_detail = '漂白できません。'
+
+            return render_template('index.html',name=name, img_url=img_url, probability=probability, pre_img_url=pre_img_url, pre_detail=pre_detail )
+    else:
+        # ターミナル及びコマンドプロンプトに出力するメッセージ
+        print("get request")
+
+    return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/seeds/<filename>')
+def seed_file(filename):
+    return send_from_directory(app.config['SEEDS_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5000)
